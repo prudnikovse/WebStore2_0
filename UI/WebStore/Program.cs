80e8e36726b563ceb4912423d98aa01defbfb3a2
sprudnikov <sprudnikov@yandex.ru>
2020-03-03 17:09:15 +0300

    Добавлен Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
+using System;
 
 namespace WebStore
 {
@@ -21,6 +24,18 @@ namespace WebStore
                 //    log.AddConsole(opt => opt.IncludeScopes = true);
                 //    log.AddDebug();
                 //})
-                .UseStartup<Startup>();
+                
+                .UseStartup<Startup>()
+                .UseSerilog((host, log) =>
+                {
+                    log.ReadFrom.Configuration(host.Configuration)
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Error)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                    .WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                    .WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+                    .WriteTo.Seq("http://localhost:5341");
+                });
     }
 }
